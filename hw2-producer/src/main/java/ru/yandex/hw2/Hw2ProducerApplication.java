66739d24a4db0287307140f0628f8be5a2d8024c
Andrey Mitkin <103187393+Andreyspb82@users.noreply.github.com>
2025-02-19 23:09:07 +0300

    Add files via upload

---
+++ hw2-producer/src/main/java/ru/yandex/hw2/Hw2ProducerApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw2ProducerApplication.class, args);
+	}
+
+}
---
+++ hw2-producer/src/main/java/ru/yandex/hw2/controller/SendBlockedController.java
@@ -0,0 +1,31 @@
+package ru.yandex.hw2.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.hw2.model.Message;
+import ru.yandex.hw2.service.SendBlockedService;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping(path = "/blocked")
+@AllArgsConstructor
+public class SendBlockedController {
+
+    private final SendBlockedService sendBlockedService;
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void sendMessage(@RequestBody List<Message> blocked) {
+
+        log.info("POST/ blockedList: {}", blocked);
+        sendBlockedService.sendBlocked(blocked);
+    }
+}
